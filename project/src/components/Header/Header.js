3a0e537aa5b38eaab7e6120c4cc4536cf26dafda
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-11 12:22:06 -0300

    update header

--- project/src/components/Header/Header.js
+++ project/src/components/Header/Header.js
@@ -4,12 +4,42 @@ import React from 'react';
 import ListNav from './ListNav';
 import ProfileImage from './ProfileImage';
 
+//  importStyles
+import styled from 'styled-components'
+const HeaderBackground = styled.div`
+    display:flex;
+    justify-content: space-between;
 
-export default function Header(){
-    return(
+    position: relative;
+    top: 0px;
+    left: 0px;
+    right: 0px;
+    z-index: 100;
+    height: 80px;
+    background: #fff;
+    -webkit-transition: all .5s ease 0s;
+    -moz-transition: all .5s ease 0s;
+    -o-transition: all .5s ease 0s;
+    transition: all .5s ease 0s;
+
+    width: 100%;
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+
+    max-width: 1140px;
+    `
+
+
+
+export default function Header() {
+    return (
         <>
-            <ProfileImage />
-            <ListNav />
+            <HeaderBackground>
+                <ProfileImage />
+                <ListNav />
+            </HeaderBackground>
         </>
     )
 }
