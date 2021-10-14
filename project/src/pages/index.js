4a058213a330f2efdf42679a565bbfa0acd25ea7
Rafael Pinto da Silva <dasilva.rafaelpt@gmail.com>
2021-10-14 11:11:41 -0300

    -- update Index

--- project/src/pages/index.js
+++ project/src/pages/index.js
@@ -1,15 +1,15 @@
 
 // importFolders
-import Header from "../components/Header/Header";
-import MainBanner from "../components/MainBanner/MainBanner";
-import AmazingEvents from '../components/AmazingEvents/AmazingEvents'
+import Header from "../../src/components/Templates/Header/Header";
+import MainBanner from "../components/Templates/MainBanner/MainBanner";
+// import AmazingEvents from '../components/AmazingEvents/AmazingEvents'
 
 export default function Home() {
   return (
     <>
     <Header />
     <MainBanner />
-    <AmazingEvents />
+    {/* <AmazingEvents /> */}
     </>
   )
 }
